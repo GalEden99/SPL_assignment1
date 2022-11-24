a4fe3f8d3135ccea18dd6731f97d60b812b879dd
Gal <galeden99@gmail.com>
2022-11-24 22:49:53 +0200

    Thursday 22:50

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 make
 
-#bin/cRace tests/01.json
-#bin/cRace tests/02.json
- bin/cRace tests/03.json
+bin/cRace tests/01.json
+bin/cRace tests/02.json
+bin/cRace tests/03.json
 
 
 
-#diff -s tests/01_sol.out tests/01.out
-#diff -s tests/02_sol.out tests/02.out
+diff -s tests/01_sol.out tests/01.out
+diff -s tests/02_sol.out tests/02.out
 diff -s tests/03_sol.out tests/03.out
 
 
